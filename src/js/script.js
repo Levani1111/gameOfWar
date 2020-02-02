cc5184618f7db6cf257e181664ce584c55f6e94f
Levani Papashvili <papashvililevani@gmail.com>
2020-02-01 22:06:30 -0500

    added deck information and changed formatting

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 // Building a class for cards using a constructor
 // Will add new values to this once I create a deck
+// making georgian language parts
 class Card {
-  constructor(value, suites) {
-    this.suites = ["Spades / yvavi ", "Hearts / guli", "Diamonds / aguri", "Clubs / jvari"];
+  constructor(value, suit) {
+    this.suits = ["Spades / kvavi", "Hearts/ guli", "Diamonds/ aguri", "Clubs/ jvari"];
     this.values = [null,null,"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"];
     this.value = value;
     this.suit = suit;
@@ -11,8 +12,8 @@ class Card {
   lessThan(card2) {
     if (this.value < card2.value) {
       return true;
-    } else if (this.value == card2.value) {
-      return this.suites < card2.suites;
+    } else if (this.value === card2.value) {
+      return this.suit < card2.suit;
     }
     return false;
   }
@@ -20,13 +21,45 @@ class Card {
   greaterThan(card2) {
     if (this.value > card2.value) {
       return true;
-    } else if (this.value == card2.value) {
-      return this.suites > card2.suites;
+    } else if (this.value === card2.value) {
+      return this.suit > card2.suit;
     }
     return false;
   }
 
   print() {
-    return this.values[this.value] + " of " + this.suits[this.suitees];
+    return this.values[this.value] + " of " + this.suits[this.suit];
+  }
+}
+//Building a deck class
+
+class Deck {
+  constructor() {
+    this.cards = [];
+    for (let i = 2; i <= 14; i++) {
+      for (let j = 0; j <= 3; j++) {
+        let card = new Card(i, j);
+        this.cards.push(card.print());
+      }
+    }
+    this.shuffle(this.cards);
+    this.shuffle(this.cards);
+  }
+
+  removeCard() {
+    if (this.cards.length === 0) {
+      return;
+    }
+    return this.cards.pop();
+  }
+
+  shuffle() {
+    for (let i = 0; i <= 51; i++) {
+      var j = Math.floor(Math.random() * (i + 1));
+      let temp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = temp;
+    }
+    return this.cards;
   }
 }
