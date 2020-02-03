2e96ef9c4ffd49b8186148f5b453aa1b53b8bdb2
Levani Papashvili <papashvililevani@gmail.com>
2020-02-02 20:41:57 -0500

    Added game code text and tested

--- src/js/script.js
+++ src/js/script.js
@@ -76,4 +76,64 @@ class Player {
   }
 }
 
+// build the game
+
+
+class Game {
+  constructor() {
+    let name1 = prompt("What is Player 1's Name?");
+    let name2 = prompt("What is Player 2's Name?");
+    this.deck = new Deck();
+    this.player1 = new Player(name1);
+    this.player2 = new Player(name2);
+  }
+
+  wins(winner) {
+    console.log(`${winner} is the winner this round!!! Congratulations!`);
+  }
+
+  draw(player1Name, player1Card, player2Name, player2Card) {
+    console.log(
+      `${player1Name} drew ${player1Card} and ${player2Name} drew ${player2Card}`
+    );
+  }
+
+  playGame() {
+    let cards = this.deck.cards;
+    console.log("Let the War Begin! FIRE!!!!");
+    while (cards.length >= 2) {
+      
+      let player1Card = this.deck.removeCard();
+      let player2Card = this.deck.removeCard();
+      let player1Name = this.player1.name;
+      let player2Name = this.player2.name;
+      this.draw(player1Name, player1Card, player2Name, player2Card);
+      if (player1Card > player2Card) {
+        this.player1.wins++;
+        this.wins(this.player1.name);
+      } 
+// insert WAR else if statement to address ties
+      else {
+        this.player2.wins++;
+        this.wins(this.player2.name);
+      }
+    }
+    let win = this.winner(this.player1, this.player2);
+    console.log(`The war is over! ${win} is the winner! Congratulations ${win} !`);
+  }
+
+  winner(player1, player2) {
+    if (player1.wins === player2.wins) {
+      return "It was a tie!";
+    } else {
+      return player1.wins > player2.wins ? player1.name : player2.name;
+    }
+  }
+}
+//constitute the game
+const game = new Game();
+game.playGame();
+
+
+
 
