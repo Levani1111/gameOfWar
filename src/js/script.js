08a765fa852fcf62fc7d082c53e1a2f0e86ca28d
Levani Papashvili <papashvililevani@gmail.com>
2020-03-26 21:36:03 -0400

    Updated variables used in game

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
 // Building a class for cards using a constructor
 // Will add new values to this once I create a deck
 // making georgian language parts
+// define players
 let player1 = [];
 let player2 = [];
+//define card sets
 let cardsSet1 = [];
 let cardsSet2 = [];
 
@@ -27,10 +29,10 @@ class Deck {
       "spade - jvari"
     ];
     let ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
-    let scores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
     for (let i = 0; i < suits.length; i++) {
       for (let j = 0; j < ranks.length; j++) {
-        this.cards.push(new Card(suits[i], ranks[j], scores[j]));
+        this.cards.push(new Card(suits[i], ranks[j], values[j]));
       }
     }
   }
@@ -65,10 +67,39 @@ mainDeck.shuffle();
 // deal the cards
 mainDeck.deal();
 // game works to compare cards
-function compare(){
-    if(cardsInPlay1[0].score > cardsInPlay2[0].score){
-        player1.push(cardsInPlay1[0], cardsInPlay2[0]);
-        cardsInPlay1 = [];
-        cardsInPlay2 = [];
-        cardsInPlay3 = [];
-        console.log(`Player 1 wins this round! Player 1 has ${player1.length} cards! Player 2 has ${player2.length} cards!`)
+function compare() {
+  if (cardsSet1[0].value > cardsSet2[0].value) {
+    player1.push(cardsSet1[0], cardsSet2[0]);
+    cardsSet1 = [];
+    cardsSet2 = [];
+    console.log(
+      `Player 1 wins this round! Player 1 has ${player1.length} cards! Player 2 has ${player2.length} cards!`
+    );
+  } else if (cardsSet1[0].value < cardsSet2[0].value) {
+    player2.push(...cardsSet2, ...cardsSet1);
+    cardsSet1 = [];
+    cardsSet2 = [];
+    console.log(
+      `Player 2 wins this war! Player 2 has ${player2.length} cards! Player 1 has ${player1.length} cards!`
+    );
+  } else {
+    war();
+    console.log("I declare war against you! DIE DIE DIE!!!");
+  }
+}
+// define the war function
+
+function war(){
+    cardsInPlay1.unshift(...player1.splice(0,4));
+    cardsInPlay2.unshift(...player2.splice(0,4));
+    compareWar();
+};
+
+// define flipping cards
+
+function flipCard() {
+  cardsInPlay1.push(player1[0]);
+  player1.shift();
+  cardsInPlay2.push(player2[0]);
+  player2.shift();
+}
